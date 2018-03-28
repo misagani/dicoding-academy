b53e0ec9cedef59f28f0fc1ca6273d4d3535f174
Gani <gani@misagani.com>
2018-03-28 21:59:12 +0700

    BasicScripting
    Latihan Pertama

---
+++ Belajar Membuat Game Untuk Pemula/BasicScripting/Assets/Latihan2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan2 : MonoBehaviour {
+	// Global Variables
+	public int nilaiA;
+	public int nilaiB;
+    [Range(1, 5)]
+    public int nilaiC;
+	
+	// Use this for initialization
+	void Start () {
+        int total = (nilaiA + nilaiB) * nilaiC;
+        Debug.Log("Total " + total);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
